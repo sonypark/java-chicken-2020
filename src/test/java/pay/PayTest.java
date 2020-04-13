aa175f77df5fe24216ece5e52a69951f4963361b
Sony <sonypark0204@gmail.com>
2020-04-13 16:03:34 +0900

    test: 치킨 10개 단위 할인 테스트 추가

--- src/test/java/pay/PayTest.java
+++ src/test/java/pay/PayTest.java
@@ -4,9 +4,14 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import domain.Menu;
 import domain.MenuRepository;
@@ -16,12 +21,17 @@ import domain.Table;
 class PayTest {
     private static final int CREDIT_CARD = 1;
     private static final int CASH = 2;
+    private final List<Menu> menus = MenuRepository.menus();
+
+    @BeforeEach
+    void setUp() {
+
+    }
 
     @Test
     @DisplayName("주문 내역 카드 계산")
     void calculateByCreditCard() {
         //given
-        List<Menu> menus = MenuRepository.menus();
         Menu menu = menus.get(0);
         OrderMenu orderMenu = new OrderMenu(menu, new Table(1), 1);
 
@@ -33,7 +43,6 @@ class PayTest {
     @DisplayName("주문 내역 현금 계산")
     void calculateByCash() {
         //given
-        List<Menu> menus = MenuRepository.menus();
         Menu menu = menus.get(0);
         OrderMenu orderMenu = new OrderMenu(menu, new Table(1), 1);
 
@@ -45,11 +54,30 @@ class PayTest {
     @DisplayName("주문 내역 없을 때 계산")
     void calculate_when_no_order() {
         //given
-        List<Menu> menus = MenuRepository.menus();
         Menu menu = menus.get(0);
         OrderMenu orderMenu = new OrderMenu(menu, new Table(1), 0);
 
         //when, then
         assertThat(new Pay().getPayment(CASH, Arrays.asList(orderMenu))).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @DisplayName("치킨 10개 단위로 할인")
+    @MethodSource("provideChicken")
+    void calculate_chicken_discount(final int PAYMENT_TYPE, int amount, int expected) {
+        Menu menu = menus.get(0);
+        OrderMenu orderMenu = new OrderMenu(menu, new Table(1), amount);
+
+        //when, then
+        assertThat(new Pay().getPayment(PAYMENT_TYPE, Arrays.asList(orderMenu))).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideChicken() {
+        return Stream.of(
+            Arguments.arguments(CREDIT_CARD, 10, 150_000),
+            Arguments.arguments(CASH, 10, 142_500),
+            Arguments.arguments(CREDIT_CARD, 9, 144_000),
+            Arguments.arguments(CASH, 9, 136_800)
+        );
     }
 }
