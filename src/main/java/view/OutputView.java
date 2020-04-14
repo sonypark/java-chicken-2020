ba9487f214fc4ebf5770c4421ffc5f2e03dc36c1
Sony <sonypark0204@gmail.com>
2020-04-14 11:31:29 +0900

    feat: InputView, OutputView 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,21 +1,23 @@
 package view;
 
+import java.util.List;
+
 import domain.Menu;
 import domain.Table;
-
-import java.util.List;
 
 public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String ORDER_BOTTOM_LINE = "└ ₩ ┘";
+    private static final String MAIN_SCREEN_LIST = "## 메인화면\n1 - 주문등록\n2 - 결제하기\n3 - 프로그램 종료\n";
 
     public static void printTables(final List<Table> tables) {
         System.out.println("## 테이블 목록");
         final int size = tables.size();
         printLine(TOP_LINE, size);
         printTableNumbers(tables);
-        printLine(BOTTOM_LINE, size);
+        // printTableBottomLine(tables);
     }
 
     public static void printMenus(final List<Menu> menus) {
@@ -36,5 +38,36 @@ public class OutputView {
             System.out.printf(TABLE_FORMAT, table);
         }
         System.out.println();
+    }
+    //
+    // private static void printTableBottomLine(final List<Table> tables) {
+    //     for (final Table table : tables) {
+    //         if (table.hasOrder()) {
+    //             System.out.print(ORDER_BOTTOM_LINE);
+    //             continue;
+    //         }
+    //         System.out.print(BOTTOM_LINE);
+    //     }
+    //     System.out.println();
+    // }
+    //
+    // public static void printMainScreen() {
+    //     System.out.println(MAIN_SCREEN_LIST);
+    // }
+    //
+    // public static void printOrderList(Collection<Order> orderMenuList) {
+    //     System.out.println("## 주문 내역");
+    //     System.out.println("메뉴 수량 금액");
+    //     orderMenuList.forEach(
+    //         order -> System.out.println(
+    //             order.getMenu().getName() + " " + order.getAmount() + " " + (int)order.price()));
+    // }
+
+    public static void printPaymentTableNumber(int tableNumber) {
+        System.out.println(tableNumber + "번 테이블의 결제를 진행합니다.");
+    }
+
+    public static void printPaymentResult(double payment) {
+        System.out.println("최종 결제한 금액은 " + (int)payment + "원 입니다.");
     }
 }
